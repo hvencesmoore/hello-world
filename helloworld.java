ad0045b1a0c3e0d6154d43709771d4d76d9a5c2b
hvencesmoore <hvences0@email.cpcc.edu>
2026-02-16 13:11:23 -0500

    Update helloworld.java
    added reference and primitive variable

--- helloworld.java
+++ helloworld.java
@@ -4,4 +4,15 @@ public class helloworld
    {
       System.out.println("Hello World!");
       }
-}
+}      
+      // name is a reference variable
+      String name = "Tatertot";
+      
+      System.out.println(name);
+      
+      // age is a primitive variable
+      int age =3; 
+      
+      System.out.println(age);
+      }// end main
+}// end class
